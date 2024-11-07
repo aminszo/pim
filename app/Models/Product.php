cfa9c37ba3ef5fe1acf7f32a75c91003fe6b1698
amin <amin.salehizade@gmail.com>
2024-11-07 16:01:03 +0100

    Use cache for getStats() function in 'Products' Model
    - Use cache for products stats to reduce number of database queries
    
    - Add Laravel Debugbar

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 class Product extends Model
 {
     use HasFactory;
+
+    const STATS_CACHE_KEY = 'products_stats';
 
     protected $fillable = [
         'code',
@@ -33,27 +36,52 @@ class Product extends Model
         'description',
     ];
 
+    public static function boot()
+    {
+        parent::boot();
+
+        // Clear cache on product creation
+        static::created(function () {
+            Cache::forget(self::STATS_CACHE_KEY);
+        });
+
+        // Clear cache on product update
+        static::updated(function () {
+            Cache::forget(self::STATS_CACHE_KEY);
+        });
+
+        // Optionally, clear cache on product deletion
+        static::deleted(function () {
+            Cache::forget(self::STATS_CACHE_KEY);
+        });
+    }
+
     public static function getStats()
     {
-        $stats = [
-            'all' => self::count(),
-            'in-stock' => self::where('stock_status', 'in-stock')->count(),
-            'out-of-stock' => self::where('stock_status', 'out-of-stock')->count(),
-            'cordless' => self::where('type', 'cordless')->count(),
-            'corded' => self::where('type', 'corded')->count(),
-            'hybrid' => self::where('type', 'hybrid')->count(),
-            'panasonic' =>  [
-                'all' => self::where('brand', 'پاناسونیک')->count(),
-                'in-stock' => self::where('brand', 'پاناسونیک')->where('stock_status', 'in-stock')->count(),
-                'out-of-stock' => self::where('brand', 'پاناسونیک')->where('stock_status', 'in-stock')->count(),
-            ],
-            'siemens' => [
-                'all' =>  self::where('brand', 'زیمنس')->count(),
-                'in-stock' => self::where('brand', 'زیمنس')->where('stock_status', 'in-stock')->count(),
-                'out-of-stock' => self::where('brand', 'زیمنس')->where('stock_status', 'in-stock')->count(),
-            ],
-        ];
 
-        return $stats;
+        // set a cache for stats with 720 minuts lifetime (12 hours).
+        // this will return the cached value if its available and if its not available, store it in cache.
+        return Cache::remember(self::STATS_CACHE_KEY, 720, function () {
+            $stats = [
+                'all' => self::count(),
+                'in-stock' => self::where('stock_status', 'in-stock')->count(),
+                'out-of-stock' => self::where('stock_status', 'out-of-stock')->count(),
+                'cordless' => self::where('type', 'cordless')->count(),
+                'corded' => self::where('type', 'corded')->count(),
+                'hybrid' => self::where('type', 'hybrid')->count(),
+                'panasonic' =>  [
+                    'all' => self::where('brand', 'پاناسونیک')->count(),
+                    'in-stock' => self::where('brand', 'پاناسونیک')->where('stock_status', 'in-stock')->count(),
+                    'out-of-stock' => self::where('brand', 'پاناسونیک')->where('stock_status', 'in-stock')->count(),
+                ],
+                'siemens' => [
+                    'all' =>  self::where('brand', 'زیمنس')->count(),
+                    'in-stock' => self::where('brand', 'زیمنس')->where('stock_status', 'in-stock')->count(),
+                    'out-of-stock' => self::where('brand', 'زیمنس')->where('stock_status', 'in-stock')->count(),
+                ],
+            ];
+
+            return $stats;
+        });
     }
 }
